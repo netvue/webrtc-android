2ee3fbbfeb38c758fc036e32830df38587ceac18
Docee <docee@163.com>
2022-04-12 12:33:22 +0800

    Release M88.01

---
+++ README.md
@@ -0,0 +1,27 @@
+# android
+
+[![](https://jitpack.io/v/webrtc-sdk/android.svg)](https://jitpack.io/#webrtc-sdk/android)
+
+WebRTC pre-compiled library for android.
+
+## How to use
+
+Add it in your root build.gradle at the end of repositories:
+
+```gradle
+  allprojects {
+    repositories {
+      ...
+      maven { url 'https://jitpack.io' }
+    }
+  }
+```
+
+Step 2. Add the dependency
+
+
+```gradle
+  dependencies {
+    implementation 'com.github.netvue:webrtc-android:M88.01'
+  }
+```
---
+++ custom_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+VERSION=M88.01
+
+RELEASE_VERSION=${VERSION}
+
+SDK_BIN_URL=https://github.com/netvue/webrtc-android/releases/download/${VERSION}/libwebrtc.aar
+
+BUILD_DIR=build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+echo "Downloading webrtc-sdk ${VERSION} binary for android."
+curl -L -O ${SDK_BIN_URL}
+
+mvn install:install-file \
+    -Dfile=libwebrtc.aar \
+    -Dpackaging=aar \
+    -Dversion=${RELEASE_VERSION} \
+    -DgroupId=com.github.netvue \
+    -DartifactId=webrtc-android
